9053b9df575cce679370baec9a6dfe3a2df1a562
chaosd3v <saurabhjg101@gmail.com>
2022-02-27 18:55:05 +0530

    Updated showAge function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@ const calculateBtn = document.getElementById("calculate")
 const resetBtn = document.getElementById("reset")
 const resultContent = document.getElementById("result")
 
-calculateBtn.addEventListener('click', function(){
-    let age = calculateAge(birthDate.value)
-    resultContent.innerText = `Your age is ${age}`
+calculateBtn.addEventListener('click', function() {
+   showAge()
 })
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', function() {
     clearResult()
 })
 
 function calculateAge(dob) {
+    //TODO Refactor the code
     const DEFAULT_UTC_YEAR = 1970    
     let birthTime = new Date(dob).getTime()
     //calculate month difference from current date in time  
@@ -25,10 +25,22 @@ function calculateAge(dob) {
     let year = age_dt.getUTCFullYear();  
           
     //now calculate the age of the user  
-    let age = Math.abs(year - DEFAULT_UTC_YEAR);  
+    let age = Math.abs(year - DEFAULT_UTC_YEAR); 
 
     return age;
 }
+
+function showAge(){
+    let age = calculateAge(birthDate.value)
+    if(isNaN(age)){
+        resultContent.innerHTML = `<span style="color:red;">Birthdate is required.</span>`
+    }else if(age === 0){
+        resultContent.innerHTML = `<span style="color:red;">Birth year should not be current year.</span>`
+    }else{
+        resultContent.innerText = `Your age is ${age}`
+    }
+}
+
 
 function clearResult() {
     resultContent.innerText = ''
